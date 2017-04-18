180086fefcc5ee4a08531b4fa3ddeaf351c2f4b4
Ujjwal Sharma <usharma1998@gmail.com>
2017-04-18 22:36:59 +0530

    Hackathon 2017

--- js/script.js
+++ js/script.js
@@ -1,50 +1,24 @@
-$(window).scroll(function() {
-  const scroll = $(this).scrollTop();
-
-  $('.box-header').css({
-    'transform': 'translate(0px, -' + scroll / 6 + '%)',
-    'transition': 'fadeOut 0.2s'
-  });
-
-  $('.fa-angle-double-down').css({
-    'transform': 'translate(0px, ' + scroll / 2 + '%)'
-  });
+$('.bars').click(function() {
+  $('.navpane').animate({'left': '+=280px'}, 200);
 });
 
-$('body').on('click', '.fa-bars', function() {
-  $('.navpane').animate({
-    left: '+=280px'
-  }, 200);
-  $('.fa-bars').animate({
-    left: '+=280px'
-  }, 200, function() {
-    $(this).addClass('fa-times');
-    $(this).removeClass('fa-bars');
-  });
+$('.close').click(function() {
+  $('.navpane').animate({'left': '-=280px'}, 200);
 });
 
-$('body').on('click', '.fa-times', function() {
-  $('.navpane').animate({
-    left: '-=280px'
-  }, 200);
-  $('.fa-times').animate({
-    left: '-=280px'
-  }, 200, function() {
-    $(this).addClass('fa-bars');
-    $(this).removeClass('fa-times');
-  });
-});
-
-$(function(){
-  $(".typewriter").typed({
-    strings: [
-      '<span class="dev">development</span>', 
-      '<span class="des">Design<span>', 
-      '<span class="sec">security</span>', 
-      '<span class="lin">Linux</span>', 
-      '<span class="oss">Open Source</span>^2000'
-    ],
-    typeSpeed: 0,
-    loop: true
-  });
+Typed.new(".typewriter", {
+  strings: [
+    'Development',
+    'Design',
+    'Security',
+    'Linux',
+    'Open Source',
+    'Free',
+    'Hacker',
+    'Cypherpunk'
+  ],
+  typeSpeed: 0,
+  loop: true,
+  shuffle: true,
+  showCursor: false
 });
